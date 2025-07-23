c1d6972089f033bb599374bea62a6f806b6f089d
Muhammad Asad Janjua <asadjanjua12@live.com>
2025-07-23 14:33:44 +0400

    Enable password reset in admin

--- pagasys/admin.py
+++ pagasys/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import (
     Company,
@@ -16,5 +18,46 @@ admin.site.register(Designation)
 admin.site.register(TradeLicense)
 admin.site.register(Department)
 admin.site.register(Project)
-admin.site.register(Employee)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(UserAdmin):
+    """Admin configuration for Employee model with password reset."""
+
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = Employee
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Employment Info",
+            {
+                "fields": (
+                    "trade_license",
+                    "department",
+                    "project",
+                    "designation",
+                    "hire_date",
+                    "employment_type",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Employment Info",
+            {
+                "fields": (
+                    "trade_license",
+                    "department",
+                    "project",
+                    "designation",
+                    "hire_date",
+                    "employment_type",
+                )
+            },
+        ),
+    )
+
 
