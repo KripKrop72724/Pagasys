31c895fc4b69cea0469f36eea0884af7250c146d
Muhammad Asad Janjua <asadjanjua12@live.com>
2025-07-25 20:47:26 +0400

    Fix superuser group handling and JS

--- pagasys/static/pagasys/js/employee_admin.js
+++ pagasys/static/pagasys/js/employee_admin.js
@@ -1,11 +1,21 @@
-(function($) {
+(function(factory) {
+  var $ = window.django && window.django.jQuery ? window.django.jQuery : window.jQuery;
+  if ($) {
+    factory($);
+  } else {
+    document.addEventListener('DOMContentLoaded', function() {
+      var $ = window.django && window.django.jQuery ? window.django.jQuery : window.jQuery;
+      if ($) factory($);
+    });
+  }
+})(function($) {
   function toggleGroupField() {
     var isSuper = $('#id_is_superuser').is(':checked');
     $('#id_groups').closest('.form-row').toggle(!isSuper);
     $('#id_groups').prop('disabled', isSuper);
   }
-  $(document).ready(function() {
-    $('#id_is_superuser').change(toggleGroupField);
+  $(function() {
+    $('#id_is_superuser').on('change', toggleGroupField);
     toggleGroupField();
   });
-})(django.jQuery);
+});
