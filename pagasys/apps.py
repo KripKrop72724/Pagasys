9ac1d50653abec7fdb942d3de79567ba5503a8d2
Muhammad Asad Janjua <asadjanjua12@live.com>
2025-07-25 22:35:45 +0400

    Brand Django admin as Pagasys

--- pagasys/apps.py
+++ pagasys/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.contrib import admin
 
 
 class PagasysConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pagasys"
+
+    def ready(self):
+        """Brand the Django admin with the Pagasys name."""
+        admin.site.site_header = "Pagasys Payroll administration"
+        admin.site.site_title = "Pagasys Payroll admin"
+        admin.site.index_title = "Pagasys Payroll administration"
+
